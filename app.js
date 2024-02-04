91eb4969a6ed75d867ee8c4a517810a166922fa5
RicardoRpo <ricar2011.medallo@gmail.com>
2024-02-04 18:22:09 -0500

    Version final número secreto

---
+++ app.js
@@ -0,0 +1,61 @@
+let numeroSecreto = 0;
+let intentos = 0;
+let listasNumerosSorteados = [];
+let numeroMaximo = 10;
+
+function asignarTextoElemento (elemto, texto) {
+    let elementoHTML = document.querySelector(elemto);
+    elementoHTML.innerHTML = texto;
+    return;
+}
+
+function verificarIntento(){
+    let numeroDeUsuario = parseInt(document.getElementById('valorUsuario').value);
+    if (numeroDeUsuario === numeroSecreto){
+        asignarTextoElemento('p', `Acertaste el número en ${intentos} ${(intentos === 1)? 'vez' : 'veces'}`);
+        document.getElementById('reiniciar').removeAttribute('disabled');
+    }else {
+        if(numeroDeUsuario > numeroSecreto){
+            asignarTextoElemento('p', 'El número secreto es menor');
+        }else {
+            asignarTextoElemento('p', 'El número secreto es Mayor');
+        }
+        intentos++;
+        limpiarCaja();
+    }
+    return;
+}
+
+function limpiarCaja(){
+    document.querySelector('#valorUsuario').value = '';
+}
+
+function generarNumeroSecreto(){
+    let numeroGenerado = Math.floor(Math.random()*numeroMaximo)+1;   
+    if(listasNumerosSorteados.length == numeroMaximo){
+        asignarTextoElemento('p','Ya se sortearon todos los numeros posibles');
+    }else{
+        if (listasNumetoSorteados.includes(numeroGenerado)) {
+            return generarNumeroSecreto();
+        }else{
+            listasNumerosSorteados.push(numeroGenerado);
+            return numeroGenerado;
+        }
+    }
+    
+}
+
+function  condicionesIniciales() {
+    asignarTextoElemento('h1', 'Juego del número secreto');
+    asignarTextoElemento('p', 'Indica un numero del 1 al 10');
+    numeroSecreto = generarNumeroSecreto();
+    intentos = 1;
+}
+
+function reiniciarJuego() {
+    limpiarCaja();
+    condicionesIniciales();
+    document.querySelector('#reiniciar').setAttribute('disabled', 'true');
+}
+
+condicionesIniciales();
